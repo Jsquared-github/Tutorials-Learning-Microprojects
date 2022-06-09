535b45150d154b3a8de1c06c9e68d9a6e4b74bd5
Joshua Johnston <2021johnston.joshua@gmail.com>
2022-06-09 16:31:23 -0400

    Remove Duplicate File

--- JavaScript/Practice Challenges/Object Render/object-render.js
+++
@@ -1,8 +0,0 @@
-let personEl = document.getElementById('description');
-let person = {
-    name: "Jorgen ",
-    age: 19,
-    country: "Sweden"
-};
-
-personEl.textContent = person.name + "is " + person.age + " years old and lives in " + person.country;
--- JavaScript/Practice Challenges/Rendering an Error/rendering-an-error.js
+++
@@ -1,5 +0,0 @@
-let errorMessage= document.getElementById('error-msg');
-
-function error(){
-    errorMessage.innerText = "Something went wrong, please try again";
-}
