5c9c5d656520bb2bac7e9e5863a1a4956e96d597
Joshua Johnston <2021johnston.joshua@gmail.com>
2022-06-17 12:11:06 -0400

    State Capital Lookup Complete

---
+++ Vanilla JavaScript/Microprojects/Auto Complete App/js/main.js
@@ -0,0 +1,41 @@
+const search = document.getElementById("search");
+const list = document.getElementById("match-list");
+
+//Search states.json and filter
+
+const searchStates = async (searchText) => {
+  const res = await fetch("../data/states.json");
+  const states = await res.json();
+
+  //Get matches to current text input
+
+  if (searchText.length === 0) {
+    matches = [];
+    console.log(matches);
+  } else {
+    let matches = states.filter((state) => {
+      const regex = new RegExp(`^${searchText}`, "gi");
+      return state.name.match(regex) || state.abbr.match(regex);
+    });
+    //Show results in html
+
+    console.log(matches);
+    const html = matches
+      .map(
+        (match) =>
+          `
+        <div class="card card-body mb-4">
+            <h4> 
+            ${match.name} (${match.abbr}) <span class="text-primary"> ${match.capital}</span>
+            </h4>
+            <small> Lat: ${match.lat} / Long: ${match.long}</small>
+        </div>
+        `
+      )
+      .join("");
+
+    list.innerHTML = html;
+  }
+};
+
+search.addEventListener("input", () => searchStates(search.value));
