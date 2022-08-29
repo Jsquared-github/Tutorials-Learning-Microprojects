04572263a00d6dac5da0fd23b68a009f48c1d1de
Joshua Johnston <2021johnston.joshua@gmail.com>
2022-08-29 10:22:38 -0400

    Pip Added

---
+++ Python/Advanced_Topics/pip.py
@@ -0,0 +1,4 @@
+# Similar to npm, pip is Python's package manager; for windows, pip installs are global
+# To make pip installs global on windows, you must set up a virtual environment
+# To install a third party package, do -> pip install package_name
+# To upgrade a package, do -> pip install -U package_name
